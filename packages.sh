9a7c7fd7e4ef7a534e3c5ab6ab1902734dbc06d7
Andrew Gozzard <gozzardam@gmail.com>
2018-08-25 15:29:22 +0800

    Fix openjdk version, iptables interactivity, and firefox config

--- packages.sh
+++ packages.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Update package list
-# Avoid interactivity
-DEBIAN_FRONTEND=noninteractive
 # Add repositories
 # Sublime Text
 apt-get -y install apt-transport-https
@@ -14,10 +12,10 @@ apt-get -y install gnome-core
 apt-get update && apt-get -y upgrade
 apt-get -y install build-essential vim-gtk emacs sublime-text idle-python3.5 eclipse eclipse-cdt-valgrind gedit-developer-plugins gedit-plugins anjuta-extras geany-plugins clang-4.0
 # Prevent iptables-persistent interactive setup
-apt-get -y install iptables-persistent
+DEBIAN_FRONTEND=noninteractive apt-get -y install iptables-persistent
 # Official Compiler Versions
 apt-get -y install gcc-6=6.3.0-18+deb9u1
-apt-get -y install openjdk-8-jdk=8u181-b13-1-deb9u1 openjdk-8-jre=8u181-b13-1-deb9u1
+apt-get -y install openjdk-8-jdk=8u181-b13-1~deb9u1 openjdk-8-jre=8u181-b13-1~deb9u1
 apt-get -y python3.5=3.5.3-1
 # Finalise package list
 apt-get -y autoremove && apt-get -y autoclean

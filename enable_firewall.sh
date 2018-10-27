292110581052e5e29e042d8e14630e7127bbd435
gozzarda <gozzarda@users.noreply.github.com>
2018-10-27 10:28:32 +0800

    Firewall ports 53 (DNS) and 631 (UPP)

--- enable_firewall.sh
+++ enable_firewall.sh
@@ -8,11 +8,8 @@ iptables -P OUTPUT DROP
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -d 192.254.188.84/32 -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -d 167.99.29.149/32 -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --dport 631 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --dport 631 -j ACCEPT
 iptables-save > /etc/iptables/rules.v4
 
 ip6tables -F

3616c56ec9328a712f57c373b64dcf678168d016
gozzarda <gozzarda@users.noreply.github.com>
2018-10-28 11:59:10 +0800

    Allow backup DOMjudge IP through firewall

--- enable_firewall.sh
+++ enable_firewall.sh
@@ -8,8 +8,12 @@ iptables -P OUTPUT DROP
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+# contest.sppregional.org
 iptables -A OUTPUT -d 192.254.188.84/32 -p tcp -m tcp --dport 80 -j ACCEPT
+# DOMjudge
 iptables -A OUTPUT -d 167.99.29.149/32 -p tcp -m tcp --dport 80 -j ACCEPT
+# Backup DOMjudge
+iptables -A OUTPUT -d 174.138.121.10/32 -p tcp -m tcp --dport 80 -j ACCEPT
 iptables-save > /etc/iptables/rules.v4
 
 ip6tables -F

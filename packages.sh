6548cf170e368e6470c80c91ecf786397b115298
gozzarda <gozzardam@gmail.com>
2019-08-28 19:10:10 +0800

    Mention g++ explicitly

--- packages.sh
+++ packages.sh
@@ -15,7 +15,7 @@ apt-get -y install build-essential vim-gtk emacs sublime-text idle-python3.5 ecl
 # Prevent iptables-persistent interactive setup
 DEBIAN_FRONTEND=noninteractive apt-get -y install iptables-persistent
 # Official Compiler Versions
-apt-get -y install gcc=4:8.3.0-1
+apt-get -y install gcc=4:8.3.0-1 g++=4:8.3.0-1
 apt-get -y install openjdk-11-jdk=11.0.4+11-1~deb10u1 openjdk-11-jre=11.0.4+11-1~deb10u1
 apt-get -y install python3=3.7.3-1
 # Finalise package list

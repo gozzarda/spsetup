2fc7b13f12d5518b53b0701052a80d347eb0070c
Andrew Gozzard <gozzardam@gmail.com>
2019-08-29 12:52:08 +0800

    Give full path for nft

--- disable_firewall.sh
+++ disable_firewall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Install nftables if we don't have it
-if ! [ -x "$(command -v nft)"  ]; then ./get_nft.sh; fi
 # Clear the firewall ruleset
-nft flush ruleset
+/usr/sbin/nft flush ruleset
--- enable_firewall.sh
+++ enable_firewall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install nftables if we don't have it
-if ! [ -x "$(command -v nft)"  ]; then ./get_nft.sh; fi
+if ! [ -x "$(command -v /usr/sbin/nft)"  ]; then ./get_nft.sh; fi
 # Reload ruleset from file and save
-nft flush ruleset
-nft -f firewall.nft
-nft list ruleset > /etc/nftables.conf
+/usr/sbin/nft flush ruleset
+/usr/sbin/nft -f firewall.nft
+/usr/sbin/nft list ruleset > /etc/nftables.conf

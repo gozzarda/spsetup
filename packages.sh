bb1572d439be18b548c95996689e2f588223427f
gozzarda <gozzardam@gmail.com>
2019-08-29 11:13:37 +0800

    Update packages and remove iptables

--- packages.sh
+++ packages.sh
@@ -11,9 +11,9 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.
 apt-get -y install gnome-core
 # Install packages
 apt-get update && apt-get -y upgrade
-apt-get -y install build-essential vim-gtk emacs sublime-text idle-python3.5 eclipse eclipse-cdt-valgrind gedit-plugins anjuta-extras geany-plugins clang-4.0
-# Prevent iptables-persistent interactive setup
-DEBIAN_FRONTEND=noninteractive apt-get -y install iptables-persistent
+apt-get -y install build-essential vim-gtk emacs sublime-text idle-python3.7 gedit-plugins anjuta-extras geany-plugins
+# Extra Compilers
+apt-get -y install clang
 # Official Compiler Versions
 apt-get -y install gcc=4:8.3.0-1 g++=4:8.3.0-1
 apt-get -y install openjdk-11-jdk=11.0.4+11-1~deb10u1 openjdk-11-jre=11.0.4+11-1~deb10u1

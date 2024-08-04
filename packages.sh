2d80879089cecfd120e88f4178da46f0986c57dd
Andrew "Gozz" Gozzard <gozzardam@gmail.com>
2024-08-04 16:18:57 +0800

    Use Mozilla PPA not Firefox Snap

--- packages.sh
+++ packages.sh
@@ -2,6 +2,7 @@
 
 # Update package list
 apt-get update
+
 # Add repositories
 apt-get -y install software-properties-common apt-transport-https gnupg wget
 # Sublime Text
@@ -12,6 +13,14 @@ wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > p
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
+# Firefox (Disable Snap, use Mozilla PPA)
+cat << EOF_FIREFOX_NOSNAP | tee /etc/apt/preferences.d/firefox-no-snap.pref
+Package: firefox*
+Pin: release o=Ubuntu*
+Pin-Priority: -1
+EOF_FIREFOX_NOSNAP
+sudo add-apt-repository -y ppa:mozillateam/ppa
+
 # Install packages
 apt-get update && apt-get -y upgrade
 # Set up minimal-ish gnome

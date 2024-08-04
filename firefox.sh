ae28a21fed7b322a58bf013e2ba45d65a947533c
Andrew "Gozz" Gozzard <gozzardam@gmail.com>
2024-08-04 18:20:57 +0800

    Switch to Firefox ESR

--- firefox.sh
+++ firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set Firefox homepage to http://contest.sppcontests.org
-cat << EOF_FIREPREFS > /usr/lib/firefox/defaults/pref/local-settings.js
+cat << EOF_FIREPREFS > /usr/lib/firefox-esr/defaults/pref/all.corp.js
 pref("browser.startup.homepage_override.mstone", "ignore", locked);
 pref("browser.startup.homepage", "http://contest.sppcontests.org", locked);
 EOF_FIREPREFS
--- packages.sh
+++ packages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Prevent prompts about restarting services
+DEBIAN_FRONTEND=noninteractive
+
 # Update package list
 apt-get update
 
 # Add repositories
-apt-get -y install software-properties-common apt-transport-https gnupg wget
+apt-get -y install software-properties-common apt-transport-https gnupg wget ca-certificates
 # Sublime Text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
@@ -13,20 +16,11 @@ wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > p
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
-# Firefox (Disable Snap, use Mozilla PPA)
-cat << EOF_FIREFOX_NOSNAP | tee /etc/apt/preferences.d/firefox-no-snap.pref
-Package: firefox*
-Pin: release o=Ubuntu*
-Pin-Priority: -1
-EOF_FIREFOX_NOSNAP
-sudo add-apt-repository -y ppa:mozillateam/ppa
 
 # Install packages
 apt-get update && apt-get -y upgrade
-# Set up minimal-ish gnome
-apt-get -y install gnome-core
-# Firefox
-apt-get -y install firefox
+# Set up minimal-ish gnome (Firefox ESR to prevent Firefox Snap)
+apt-get -y install firefox-esr gnome-core
 # Editors and such
 apt-get -y install build-essential neovim vim-gtk emacs sublime-text code idle-python3.11 gedit-plugins geany-plugins
 # Extra Compilers
